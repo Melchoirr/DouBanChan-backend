6eb80a6718d1aff5ab888bb891c31d3b9fcceb0a
Melchoirr <109651654+Melchoirr@users.noreply.github.com>
2023-05-20 18:50:20 +0800

    homepage

--- tools/processing.py
+++ tools/processing.py
@@ -6,14 +6,3 @@ import base64
 from models.models import Picture
 
 
-def getPicture(picture):
-
-
-    # 读取图片文件内容
-    with open(picture.p_content.path, 'rb') as f:
-        image_data = f.read()
-
-    # 返回图片作为响应
-    return base64.b64encode(image_data).decode('utf-8') # type?
-
-
