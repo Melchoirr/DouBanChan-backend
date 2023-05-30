4b696adc381b4ae17f1a64a0715739822cb27a1a
mjorah7 <99737363+mjorah7@users.noreply.github.com>
2023-05-30 10:00:33 +0800

    增加了 举报

---
+++ report/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from report.views import *
+
+urlpatterns = [
+    path("add_report/", add_report),
+    path("delete_report/", delete_report),
+    path("query_single/", query_single_report),
+]
