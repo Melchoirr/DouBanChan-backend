3a19b33b2cae4769eb40e516d8656a9180ff4a1b
Melchoirr <109651654+Melchoirr@users.noreply.github.com>
2023-06-06 17:00:44 +0800

    like

--- post/views.py
+++ post/views.py
@@ -48,8 +48,19 @@ def like_post(request):
 
 def post_set_favorite(request):
     user = get_cur_user(request)
-    post = get_post_by_id(request.POST['p_id'])
+    post = get_post_by_id(request.POST['t_id'])
+    post.p_favorite += 1
+    post.save()
     user_post = UserPost(post=post, user=user, is_favorite=1)
+    user_post.save()
+
+
+def post_cancel_favorite(request):
+    user = get_cur_user(request)
+    post = get_post_by_id(request.POST['t_id'])
+    post.p_favorite -= 1
+    post.save()
+    user_post = UserPost(post=post, user=user, is_favorite=0)
     user_post.save()
 
 
