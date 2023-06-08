49c23efc83d14757ac45cb780eccb6fe89dd2644
Melchoirr <109651654+Melchoirr@users.noreply.github.com>
2023-06-08 15:36:29 +0800

    1

--- sender/views.py
+++ sender/views.py
@@ -12,7 +12,7 @@ def send_email(email, u_id):
     if platform.system() == "Linux":
         url = os.path.join("https://milimili.super2021.com:8000/sender/activate/", url)
     else:
-        url = os.path.join("http://10.193.206.15:8000/sender/activate/", url)
+        url = os.path.join("http://10.193.202.49:8000/sender/activate/", url)
     data = {'url': url}
     email_title = r"欢迎注册豆瓣酱图书影视交流平台"
     email_body = loader.render_to_string('EmailContent-register.html', data)
@@ -27,10 +27,10 @@ def activate(request, u_id):
         user = User.objects.get(u_id=u_id)
         user.is_active = True
         user.save()
-        data = {"title": "感谢注册", "message": "注册豆瓣酱图书影视交流平台成功！", "url": "http://10.193.206.15:8000/"}
+        data = {"title": "感谢注册", "message": "注册豆瓣酱图书影视交流平台成功！", "url": "http://10.193.202.49:8000/"}
         return render(request, 'EmailContent-check.html', data)
     except Exception:
-        data = {"title": "注册失败", "message": "注册豆瓣酱图书影视交流平台失败！", "url": "http://10.193.206.15:8000/"}
+        data = {"title": "注册失败", "message": "注册豆瓣酱图书影视交流平台失败！", "url": "http://10.193.202.49:8000/"}
         return render(request, 'EmailContent-check.html', data)
 
 
@@ -38,7 +38,7 @@ def find(request):
     try:
         user = User.objects.get(u_name=request.POST['u_name'])
         print(user)
-        data = {"title": "找回密码", "message": "找回密码成功！", "url": "http://10.193.206.15:8000/", "password": user.u_password}
+        data = {"title": "找回密码", "message": "找回密码成功！", "url": "http://10.193.202.49:8000/", "password": user.u_password}
         email_body = loader.render_to_string('EmailContent-find.html', data)
         msg = EmailMessage(r"z找回密码", email_body, EMAIL_HOST_USER, [user.u_email])
         msg.content_subtype = 'html'
